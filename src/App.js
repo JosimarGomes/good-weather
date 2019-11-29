4874daaabc494dea4d020ccbbdd04eb3fd45a2d5
Josimar Gomes <josimar.gomes@superlogica.com>
2019-11-28 23:13:48 -0300

    add hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import { Card } from 'antd';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background: red;
+  width: 100%;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  padding: 50px;
+  height: 100vh;
+`;
+
+const Content = styled(Card)`
+  width: 50%;
+`;
 
 function App() {
+
+  const teste = () => {
+    
+  }
+
+  useEffect(() => {
+
+    axios.get("http://api.openweathermap.org/data/2.5/weather?APPID=ebd024465e138374770235a87dbab4b7&lat=-22.852126400000003&lon=-47.204491&units=metric")
+      .then(res => console.log("veio res", res))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container>
+        <Content>
+            <h1>teste</h1>
+        </Content>
+      </Container>
   );
 }
 
